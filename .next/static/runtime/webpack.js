80ee0669d91890109bdbff12ed1e492e7d5ead43
Ender Aydin Orak <ender.orak@johnlewis.co.uk>
2020-09-17 23:01:23 +0100

    Fix pointer

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6f6f24d86205ce04e94";
+/******/ 	var hotCurrentHash = "9f2fa495ce8c476dc5a8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,13 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-8 mt-8 cursor-pointer'>
+    <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-8 mt-8'>
       <Link href='/'>
         <img
           src='/assets/images/m.png'
           alt='meftun'
           style={{ display: "inline-block", maxHeight: 50, margin: 0, padding: 0 }}
+          className='cursor-pointer'
         />
       </Link>
       .
