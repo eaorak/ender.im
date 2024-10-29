8076423a60d32cbe91d1fee1e69bef9f67c9bc13
Ender Aydin Orak <eaorak@gmail.com>
2024-10-29 21:01:17 +0000

    Fix meftun

--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,10 @@ const Header = () => {
   return (
     <h2 className='text-3xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8 mb-20 mt-10'>
       <Link href='/'>
-        <img
-          src='/assets/images/m.png'
-          width={176}
-          height={85}
-          alt='ender.im'
-          style={{ display: 'inline-block', cursor: 'pointer' }}
-        />
+        <h1 className='font-logo text-8xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'>
+          Ender.im
+        </h1>
       </Link>
-      .{' '}
     </h2>
   )
 }
